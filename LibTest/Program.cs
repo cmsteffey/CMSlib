4c9074b910ff0e3ffc8e8f00ada2b4287a608dfe
cmsteffey <65865777+cmsteffey@users.noreply.github.com>
2021-07-03 22:44:29 -0400

    Proper testing, with isInput tested

--- LibTest/Program.cs
+++ LibTest/Program.cs
@@ -4,7 +4,7 @@ using CMSlib.ConsoleModule;
 ModuleManager manager = new();
 manager.AddModule("INPUT", 0, 0, Console.WindowWidth / 2, Console.WindowHeight);
 manager.AddModule("OUTPUT", Console.WindowWidth / 2, 0, Console.WindowWidth / 2, Console.WindowHeight / 2);
-manager.AddModule("LOGGING", Console.WindowWidth / 2, Console.WindowHeight / 2, Console.WindowWidth / 2, Console.WindowHeight / 2);
+manager.AddModule("LOGGING", Console.WindowWidth / 2, Console.WindowHeight / 2, Console.WindowWidth / 2, Console.WindowHeight / 2, isInput: false);
 manager.LineEntered += async (@object, args) =>
 {
     Module? module = @object as Module;
