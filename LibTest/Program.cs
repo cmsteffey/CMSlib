527c1984e4577781c1ebdcb2fcac65b11c0f65ec
cmsteffey <65865777+cmsteffey@users.noreply.github.com>
2021-07-06 17:44:36 -0400

    Comment Modules in Program.Main

--- LibTest/Program.cs
+++ LibTest/Program.cs
@@ -6,9 +6,9 @@ StandardInputModule input = new("INPUT", 0, 0, Console.WindowWidth / 2, Console.
 StandardInputModule output = new("OUTPUT", Console.WindowWidth / 2, 0, Console.WindowWidth / 2, Console.WindowHeight / 2);
 LogModule logging = new("LOGGING", Console.WindowWidth / 2, Console.WindowHeight / 2, Console.WindowWidth / 2, Console.WindowHeight / 2);
 
-manager.Add(input);
-manager.Add(output);
-manager.Add(logging);
+manager.Add(input); //input module, full left side
+manager.Add(output); //output module, top right
+manager.Add(logging); //logger module, no input, bottom right
 
 manager.LineEntered += async (@object, args) =>
 {
