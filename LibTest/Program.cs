7d5353419f72edab1507b189eb92c7553c73e662
cmsteffey <65865777+cmsteffey@users.noreply.github.com>
2021-07-06 17:41:36 -0400

    Space out Program.Main

--- LibTest/Program.cs
+++ LibTest/Program.cs
@@ -5,12 +5,15 @@ ModuleManager manager = new();
 StandardInputModule input = new("INPUT", 0, 0, Console.WindowWidth / 2, Console.WindowHeight);
 StandardInputModule output = new("OUTPUT", Console.WindowWidth / 2, 0, Console.WindowWidth / 2, Console.WindowHeight / 2);
 LogModule logging = new("LOGGING", Console.WindowWidth / 2, Console.WindowHeight / 2, Console.WindowWidth / 2, Console.WindowHeight / 2);
+
 manager.Add(input);
 manager.Add(output);
 manager.Add(logging);
+
 manager.LineEntered += async (@object, args) =>
 {
     InputModule? module = @object as InputModule;
     module?.AddText($"{args.Line}");
 };
+
 System.Threading.Tasks.Task.Delay(-1).GetAwaiter().GetResult();
