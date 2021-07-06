d58193a714c84de679987321bdd1e518e767f02f
cmsteffey <65865777+cmsteffey@users.noreply.github.com>
2021-07-06 17:21:46 -0400

    remove "hecc" from program.cs

--- LibTest/Program.cs
+++ LibTest/Program.cs
@@ -8,7 +8,6 @@ LogModule logging = new("LOGGING", Console.WindowWidth / 2, Console.WindowHeight
 manager.Add(input);
 manager.Add(output);
 manager.Add(logging);
-logging.AddText("HECC");
 manager.LineEntered += async (@object, args) =>
 {
     InputModule? module = @object as InputModule;
