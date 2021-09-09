eeff10396f6b9eb6983ee0d4a71d53523ff01dd7
cmsteffey <65865777+cmsteffey@users.noreply.github.com>
2021-09-08 23:36:38 -0400

    clamp scrolling to full page

--- CMSlib/ConsoleModule/StandardInputModule.cs
+++ CMSlib/ConsoleModule/StandardInputModule.cs
@@ -128,7 +128,7 @@ namespace CMSlib.ConsoleModule
             lock (AddTextLock)
             {
                 int before = scrolledLines;
-                scrolledLines = Math.Clamp(scrolledLines + amt, 0, this.text.Count - 1);
+                scrolledLines = Math.Clamp(scrolledLines + amt, 0, Math.Max(text.Count - (Height - 2), 0));
 
                 if (before != 0 && scrolledLines == 0) unread = false;
                 if (before != scrolledLines) WriteOutput();
@@ -142,7 +142,7 @@ namespace CMSlib.ConsoleModule
             lock (AddTextLock)
             {
                 int before = scrolledLines;
-                scrolledLines = Math.Clamp(line, 0, text.Count - 1);
+                scrolledLines = Math.Clamp(line, 0, Math.Max(text.Count - (Height - 2), 0));
 
                 if (before != 0 && scrolledLines == 0) unread = false;
                 if (before != scrolledLines) WriteOutput();
