8428b9bfbe974dca875ae9dcf90c5de138e95285
cmsteffey <65865777+cmsteffey@users.noreply.github.com>
2021-09-08 23:44:00 -0400

    Parenthesize

--- CMSlib/ConsoleModule/LogModule.cs
+++ CMSlib/ConsoleModule/LogModule.cs
@@ -67,7 +67,7 @@ namespace CMSlib.ConsoleModule
             lock (AddTextLock)
             {
                 int before = scrolledLines;
-                scrolledLines = Math.Clamp(scrolledLines + amt, 0, Math.Max(text.Count - Height - 2, 0));
+                scrolledLines = Math.Clamp(scrolledLines + amt, 0, Math.Max(text.Count - (Height - 2), 0));
                 if (before != 0 && scrolledLines == 0)
                     unread = false;
                 if (before != scrolledLines) WriteOutput();
@@ -80,7 +80,7 @@ namespace CMSlib.ConsoleModule
             lock (AddTextLock)
             {
                 int before = scrolledLines;
-                scrolledLines = Math.Clamp(line, 0, Math.Max(text.Count - Height - 2, 0));
+                scrolledLines = Math.Clamp(line, 0, Math.Max(text.Count - (Height - 2), 0));
                 if (before != 0 && scrolledLines == 0)
                     unread = false;
                 if (before != scrolledLines) WriteOutput();
