66e6b3baf8ea0b97ea592ee21a1c17f7297adc2e
cmsteffey <65865777+cmsteffey@users.noreply.github.com>
2021-09-16 13:20:00 -0400

    Finish base of TableModule.cs

--- CMSlib/ConsoleModule/TableModule.cs
+++ CMSlib/ConsoleModule/TableModule.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CMSlib.Extensions;
 using CMSlib.Tables;
 using Microsoft.Extensions.Logging;
-
+using static CMSlib.ConsoleModule.AnsiEscape;
 namespace CMSlib.ConsoleModule
 {
     public class ListModule : BaseModule
@@ -16,7 +17,7 @@ namespace CMSlib.ConsoleModule
             LogLevel logLevel = LogLevel.Information) : base(title, x, y, width, height, logLevel)
         {
             wrapped = toWrap;
-            this.header = header ?? toWrap.GetHeader();
+            this.header = (header ?? toWrap.GetHeader()).SplitOnNonEscapeLength(width - 2).First().PadToVisibleDivisible(width - 2);
             lineCache = toWrap.GetOutputRows().ToArray();
         }
         public override void AddText(string text)
@@ -55,6 +56,21 @@ namespace CMSlib.ConsoleModule
             wrapped.ClearRows();   
         }
 
+        public void RefreshHeader()
+        {
+            this.header = (wrapped.GetHeader()).SplitOnNonEscapeLength(Width - 2).First().PadToVisibleDivisible(Width - 2);
+        }
+
+        public void RefreshLineCache()
+        {
+            lineCache = wrapped.GetOutputRows().Select(x =>
+            {
+                if (x.VisibleLength() > Width - 2)
+                    return x.SplitOnNonEscapeLength(Width - 2).First();
+                return x.PadToVisibleDivisible(Width - 2);
+            }).ToArray();
+        }
+
         internal override async Task HandleClickAsync(InputRecord record, ButtonState? before)
         {
             
@@ -67,7 +83,37 @@ namespace CMSlib.ConsoleModule
 
         protected override IEnumerable<string> ToOutputLines()
         {
-            yield break;
+            int internalHeight = Height - 3;
+            int internalWidth = Width - 2;
+            if(internalHeight < 2) yield break;
+            string displayTitle = DisplayName ?? this.Title;
+            int visLen = displayTitle.VisibleLength();
+            if (visLen > internalWidth)
+            {
+                displayTitle = displayTitle.Ellipse(internalWidth);
+                visLen = displayTitle.VisibleLength();
+            }
+
+            yield return LineDrawingMode + UpperLeftCorner + AsciiMode +
+                         (this.selected ? SgrUnderline + SgrBlinking : "") + displayTitle +
+                         (this.selected ? SgrNoUnderline + SgrNoBlinking : "") + LineDrawingMode +
+                         new string(HorizontalLine, internalWidth - visLen) + UpperRightCorner;
+            yield return VerticalLine + AsciiMode + header + LineDrawingMode + VerticalLine;
+            int lineCount = Math.Min(internalHeight, lineCache.Length);
+            int spaceCount = internalHeight - lineCount;
+            for (int i = 0; i < lineCount; i++)
+            {
+                yield return VerticalLine + AsciiMode + lineCache[scrolledLines + lineCount] + LineDrawingMode +
+                             VerticalLine;
+            }
+
+            string spaceLine = null;
+            for (int i = 0; i < spaceCount; i++)
+            {
+                spaceLine ??= VerticalLine + new string('\x20', internalWidth) + VerticalLine;
+                yield return spaceLine;
+            }
+            yield return LowerLeftCorner + new string(HorizontalLine, internalWidth) + LowerRightCorner;
         }
     }
 }
