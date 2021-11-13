b4f93e5711e19c95cfd78d2ad3e43f45b40f83cb
kangarooCoder <ksbilodeau07@gmail.com>
2021-11-13 12:12:13 -0600

    Add termios module

---
+++ CMSlib/ConsoleModule/Termios/TermiosEnums/CtrlCharSubscripts.cs
@@ -0,0 +1,17 @@
+namespace CMSlib.ConsoleModule.Termios.TermiosEnums
+{
+    public enum CtrlCharSubscripts
+    {
+        VEof   = 0,
+        VEol   = 1,
+        VErase = 3,
+        VIntR  = 8,
+        VKill  = 5,
+        VQuit  = 9,
+        VStart = 12,
+        VStop  = 13,
+        VSusP  = 10,
+        VMin   = 16,
+        VTime  = 17
+    }
+}
